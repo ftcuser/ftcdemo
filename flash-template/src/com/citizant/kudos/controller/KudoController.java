a6bb0d5ee0e047d77d2650726c586d3060a4aab0
steven wang <swang@citizant.com>
2016-08-31 13:33:43 -0400

    Add kudo

--- flash-template/src/com/citizant/kudos/controller/KudoController.java
+++ flash-template/src/com/citizant/kudos/controller/KudoController.java
@@ -2,6 +2,8 @@ package com.citizant.kudos.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +21,19 @@ public class KudoController extends BaseController {
 
 	
 	@RequestMapping("/start")
-	public ModelAndView kudoPage() {
+	public ModelAndView kudoPage(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView("tile.kudoPage");
-		List<UserBean> users = kudoService.getUsers();
-		mav.addObject("users", users);
+		UserBean user = kudoService.getUserByEmail(request.getParameter("email"));
+		mav.addObject("user", user);
+		return mav;
+	}
+	
+	@RequestMapping("/create")
+	public ModelAndView create(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView("tile.kudoPage");
+	//	UserBean fromUser = this.getL
+		UserBean user = kudoService.getUserByEmail(request.getParameter("email"));
+		mav.addObject("user", user);
 		return mav;
 	}
 }
