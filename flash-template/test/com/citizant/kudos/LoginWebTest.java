1332a96577bf3f10325776a0635fff319bb355bf
ftcuser <rupesh.kumar@citizant.com>
2017-03-04 11:26:25 -0500

    Update LoginWebTest.java

--- flash-template/test/com/citizant/kudos/LoginWebTest.java
+++ flash-template/test/com/citizant/kudos/LoginWebTest.java
@@ -14,7 +14,7 @@
 	import org.openqa.selenium.remote.DesiredCapabilities;
 
 	public class LoginWebTest {
-		private static String seleniumHub= "http://54.144.179.45:4444/wd/hub;
+		private static String seleniumHub= "http://54.144.179.45:4444/wd/hub";
 		private static String startPage = "http://50.19.179.31:9090/jenkinsmanager/index.html";
 		private  static WebDriver driver;
 		
